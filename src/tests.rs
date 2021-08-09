3ebd7e37f5fe48b336d816c03194e4866f8e10c8
rodrimati1992 <rodrimatt1985@gmail.com>
2021-08-09 19:54:13 -0300

    Updated tests to include padding.
    Improved testing for errors, included checking the error's fields.

--- src/tests.rs
+++ src/tests.rs
@@ -1,5 +1,5 @@
 use crate::{decode, decoded_len, encode, encoded_len};
-use crate::{Config, DecodeError};
+use crate::{B64CharSet, Config, DecodeError, Encoding};
 
 use rand::rngs::SmallRng;
 use rand::{Rng, SeedableRng};
@@ -9,28 +9,47 @@ fn test_encode_base64() {
     let mut rng = SmallRng::seed_from_u64(6249204433781597762);
 
     macro_rules! test_encode {
-        ($in_length:literal) => {
+        ($in_length:literal) => {{
             let cfgs = [
-                (base64::STANDARD.pad(false), Config::B64),
-                (base64::URL_SAFE.pad(false), Config::B64_URL_SAFE),
-            ];
+                (base64::STANDARD, Config::B64),
+                (base64::URL_SAFE, Config::B64_URL_SAFE),
+            ]
+            .iter()
+            .copied();
+            let pads = [false, true].iter().copied();
 
-            for (b64_cfg, cfg) in cfgs.iter().copied() {
-                const OUT_LEN_NO_PAD: usize = encoded_len($in_length, Config::B64);
+            const OUT_LEN_NO_PAD: usize = encoded_len($in_length, Config::B64.end_padding(false));
+            const OUT_LEN_PAD: usize = encoded_len($in_length, Config::B64.end_padding(true));
 
-                assert_eq!(
-                    encoded_len($in_length, Config::B64_URL_SAFE),
-                    OUT_LEN_NO_PAD
-                );
+            assert_eq!(
+                encoded_len($in_length, Config::B64_URL_SAFE.end_padding(false)),
+                OUT_LEN_NO_PAD
+            );
+
+            for ((mut b64_cfg, mut cfg), pad) in itertools::iproduct!(cfgs, pads) {
+                b64_cfg = b64_cfg.pad(pad);
+                cfg = cfg.end_padding(pad);
 
                 for _ in 0..10 {
                     let input = rng.gen::<[u8; $in_length]>();
 
-                    let mut out_no_pad = [0u8; OUT_LEN_NO_PAD];
+                    let mut out_no_pad = [0u8; OUT_LEN_PAD];
                     let written = base64::encode_config_slice(&input, b64_cfg, &mut out_no_pad);
-                    assert_eq!(OUT_LEN_NO_PAD, written);
+                    if pad {
+                        assert_eq!(OUT_LEN_PAD, written);
+                    } else {
+                        assert_eq!(OUT_LEN_NO_PAD, written);
+                    }
 
-                    let left = &encode::<OUT_LEN_NO_PAD>(&input, cfg).unwrap();
+                    let left_no_pad;
+                    let left_pad;
+                    let left: &[_] = if pad {
+                        left_pad = encode::<OUT_LEN_PAD>(&input, cfg).unwrap();
+                        &left_pad
+                    } else {
+                        left_no_pad = encode::<OUT_LEN_NO_PAD>(&input, cfg).unwrap();
+                        &left_no_pad
+                    };
                     let right = &out_no_pad[..written];
 
                     assert_eq!(
@@ -43,7 +62,7 @@ fn test_encode_base64() {
                     );
                 }
             }
-        };
+        }};
     }
 
     test_encode! {0}
@@ -61,41 +80,59 @@ fn test_decode_base64() {
     let mut rng = SmallRng::seed_from_u64(6249204433781597762);
 
     macro_rules! test_decode {
-        ($unencoded_len:expr, $encoded_length:expr) => {
+        ($unencoded_len:expr, $encoded_length:expr, $encoded_length_rup:expr) => {{
             let cfgs = [
-                (base64::STANDARD.pad(false), Config::B64),
-                (base64::URL_SAFE.pad(false), Config::B64_URL_SAFE),
-            ];
+                (base64::STANDARD, Config::B64),
+                (base64::URL_SAFE, Config::B64_URL_SAFE),
+            ]
+            .iter()
+            .copied();
+            let pads = [false, true].iter().copied();
 
-            for (b64_cfg, cfg) in cfgs.iter().copied() {
-                const DECODED_LEN: usize = decoded_len($encoded_length, Config::B64);
+            const DECODED_LEN: usize =
+                decoded_len(&[0; $encoded_length], Config::B64.end_padding(false));
 
-                assert_eq!(
-                    decoded_len($encoded_length, Config::B64_URL_SAFE),
-                    DECODED_LEN
-                );
-                assert_eq!($unencoded_len, DECODED_LEN);
+            assert_eq!(
+                decoded_len(
+                    &[0; $encoded_length],
+                    Config::B64_URL_SAFE.end_padding(false)
+                ),
+                DECODED_LEN,
+                "FOO",
+            );
+            assert_eq!($unencoded_len, DECODED_LEN, "QUX");
+
+            for ((mut b64_cfg, mut cfg), pad) in itertools::iproduct!(cfgs, pads) {
+                b64_cfg = b64_cfg.pad(pad);
+                cfg = cfg.end_padding(pad);
 
                 for _ in 0..10 {
                     let input = rng.gen::<[u8; DECODED_LEN]>();
 
-                    let mut encoded_no_pad = [0u8; $encoded_length];
+                    let mut encoded_no_pad = [0u8; $encoded_length + 4];
                     let encoded_no_pad = {
-                        let written =
+                        let written_enc =
                             base64::encode_config_slice(&input, b64_cfg, &mut encoded_no_pad);
-                        assert_eq!($encoded_length, written);
-                        &encoded_no_pad[..written]
+
+                        if pad {
+                            assert_eq!($encoded_length_rup, written_enc, "BAR0");
+                        } else {
+                            assert_eq!($encoded_length, written_enc, "BAR1");
+                        }
+
+                        &encoded_no_pad[..written_enc]
                     };
 
                     let mut decoded_no_pad = [0u8; $encoded_length];
-                    let written =
+
+                    let written_dec =
                         base64::decode_config_slice(encoded_no_pad, b64_cfg, &mut decoded_no_pad)
                             .unwrap();
 
-                    assert_eq!(DECODED_LEN, written);
+                    assert_eq!(DECODED_LEN, written_dec, "BAZ");
 
                     let left = &decode::<DECODED_LEN>(encoded_no_pad, cfg).unwrap();
-                    let right = &decoded_no_pad[..written];
+                    let right = &decoded_no_pad[..written_dec];
 
                     assert_eq!(
                         left, right,
@@ -104,23 +141,40 @@ fn test_decode_base64() {
                     );
                 }
             }
-        };
+        }};
     }
 
-    test_decode! {0, 0}
-    test_decode! {1, 2}
-    test_decode! {2, 3}
-    test_decode! {3, 4}
-    test_decode! {4, 6}
-    test_decode! {5, 7}
-    test_decode! {6, 8}
-    test_decode! {7, 10}
-    test_decode! {8, 11}
-    test_decode! {9, 12}
+    test_decode! {0, 0, 0}
+    test_decode! {1, 2, 4}
+    test_decode! {2, 3, 4}
+    test_decode! {3, 4, 4}
+    test_decode! {4, 6, 8}
+    test_decode! {5, 7, 8}
+    test_decode! {6, 8, 8}
+    test_decode! {7, 10, 12}
+    test_decode! {8, 11, 12}
+    test_decode! {9, 12, 12}
 }
 
 #[test]
 fn test_decode_base64_errors() {
+    {
+        // intentionally padded to this length
+        let ok = decode::<5>(b"BAACAAA==", Config::B64.end_padding(true)).unwrap();
+        assert_eq!(ok, [4, 0, 2, 0, 0]);
+    }
+    {
+        let ok = decode::<5>(b"BAACAAA=", Config::B64.end_padding(true)).unwrap();
+        assert_eq!(ok, [4, 0, 2, 0, 0]);
+    }
+    {
+        let ok = decode::<4>(b"BAACAA==", Config::B64.end_padding(true)).unwrap();
+        assert_eq!(ok, [4, 0, 2, 0]);
+    }
+    {
+        let ok = decode::<1>(b"BA==", Config::B64.end_padding(true)).unwrap();
+        assert_eq!(ok, [4]);
+    }
     {
         let ok = decode::<4>(b"BAACAA", Config::B64).unwrap();
         assert_eq!(ok, [4, 0, 2, 0]);
@@ -133,15 +187,37 @@ fn test_decode_base64_errors() {
     // InvalidByte
     {
         let err = decode::<3>(b"AA\x00A", Config::B64).unwrap_err();
-        assert!(matches!(err, DecodeError::InvalidByte { .. }), "{:?}", err);
+        assert!(
+            matches!(
+                &err,
+                DecodeError::InvalidByte(x)
+                if x.index() == 2 &&
+                    x.byte() == b'\x00' &&
+                    x.byte_as_char() == '\x00' &&
+                    x.encoding() == Encoding::Base64(B64CharSet::Standard)
+            ),
+            "{:?}",
+            err
+        );
     }
     {
         let err = decode::<4>(b"AAA\x00AA", Config::B64).unwrap_err();
         assert!(matches!(err, DecodeError::InvalidByte { .. }), "{:?}", err);
     }
     {
-        let err = decode::<5>(b"AAAAA\x00A", Config::B64).unwrap_err();
-        assert!(matches!(err, DecodeError::InvalidByte { .. }), "{:?}", err);
+        let err = decode::<5>(b"AAAAA=A", Config::B64_URL_SAFE).unwrap_err();
+        assert!(
+            matches!(
+                &err,
+                DecodeError::InvalidByte(x)
+                if x.index() == 5 &&
+                    x.byte() == b'=' &&
+                    x.byte_as_char() == '=' &&
+                    x.encoding() == Encoding::Base64(B64CharSet::UrlSafe)
+            ),
+            "{:?}",
+            err
+        );
     }
     {
         let err = decode::<6>(b"AAAAAAA\x00", Config::B64).unwrap_err();
@@ -176,13 +252,25 @@ fn test_decode_base64_errors() {
     {
         let err = decode::<5>(b"AAAAAAA\x00", Config::B64).unwrap_err();
         assert!(
-            matches!(err, DecodeError::MismatchedOutputLength { .. }),
+            matches!(
+                &err,
+                DecodeError::MismatchedOutputLength(x)
+                if x.expected() == 5 && x.found() == 6
+            ),
             "{:?}",
             err
         );
     }
 
     // InvalidInputLength
+    {
+        let err = decode::<5>(b"A===", Config::B64.end_padding(true)).unwrap_err();
+        assert!(
+            matches!(&err, DecodeError::InvalidInputLength(x) if x.length() == 1),
+            "{:?}",
+            err
+        );
+    }
     {
         let err = decode::<5>(b"A", Config::B64).unwrap_err();
         assert!(
